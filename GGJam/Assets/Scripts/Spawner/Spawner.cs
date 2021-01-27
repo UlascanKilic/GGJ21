ac1f77883a8b384fd237152eb2c313bec2a6720d
Ulaşcan Kılıç <ulascank1@gmail.com>
2021-01-27 20:31:50 +0300

    spawner

---
+++ GGJam/Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    public Transform spawnPoints;
+    [SerializeField]
+    public List<GameObject> collectibles;
+    [SerializeField]
+    public List<GameObject> objects;
+
+    private Vector3 targetPosition;
+
+    private List<Transform> childPoints;
+    private List<int> forbiddenIndex;
+    private int counter = 0;
+    void Start()
+    {
+        childPoints = new List<Transform>();
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        int random;
+        foreach(Transform child in spawnPoints)
+        {
+            childPoints.Add(child);
+        }
+
+        for (int i = 0; i < collectibles.Count; i++)//collectiblesleri yerleştiricem
+        {
+            random = Random.Range(0, childPoints.Count);
+            targetPosition = childPoints[random].transform.position;
+            Instantiate(collectibles[i], targetPosition, Quaternion.identity);
+            childPoints.RemoveAt(random);
+        }
+
+        for(int i = 0;i < objects.Count; i++)//kalan yerlere objectleri yerleştiricem
+        {
+            random = Random.Range(0, childPoints.Count);
+            targetPosition = childPoints[random].transform.position;
+            Instantiate(objects[i], targetPosition, Quaternion.identity);
+            childPoints.RemoveAt(random);
+        }   
+    }
+    private int ChooseRandomSpawn()
+    {
+        int random = Random.Range(0, spawnPoints.childCount);
+        if (forbiddenIndex.Contains(random) && forbiddenIndex.Count != 0)
+        {
+            ChooseRandomSpawn();
+        }
+        else
+        {
+            forbiddenIndex.Add(random);            
+        }
+        return random;
+    }
+}
